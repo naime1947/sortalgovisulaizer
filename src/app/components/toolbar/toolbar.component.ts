a58a9c39c610e50d539b2cb217e1763df0650cc4
Naime Ahmed <naime1947@gmail.com>
2022-06-22 15:32:00 +0600

    sorting visual area created and array generated

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,15 +1,22 @@
+import { ArraySortService } from './../../services/arr-sort.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
+  constructor(private arrService: ArraySortService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  generateNewArr() {
+    this.arrService.generateNewArr();
   }
 
+  onRangeChange(e: any) {
+    const value = e.target.value;
+    this.arrService.generateNewArr(value);
+  }
 }
