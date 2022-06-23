d555d51bc748fe5f167e05c54e0822d761141d7a
Naime Ahmed <naime1947@gmail.com>
2022-06-23 12:22:52 +0600

    bubble sort done

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,30 +1,82 @@
-import { ArraySortService } from './../../services/arr-sort.service';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  ArraySortService,
+  ArrayModel,
+} from './../../services/arr-sort.service';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent implements OnInit, OnDestroy {
   @ViewChild('range') range!: ElementRef;
   @ViewChild('speed') speed!: ElementRef;
+  isMergeSortSelected = false;
+  isQuickSortSelected = false;
+  isHeapSortSelected = false;
+  isBubbleSortSelected = true;
+
+  arr!: ArrayModel[];
+  arrSub!: Subscription;
+
+  isSortingStarted = false;
+  isSortingDone = false;
 
   constructor(public arrService: ArraySortService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.arrSub = this.arrService.arr$.subscribe((arr) => (this.arr = arr));
+  }
 
   generateNewArr() {
     const value = this.range.nativeElement.value;
     this.arrService.generateNewArr(value);
   }
 
-  onRangeChange(e: any) {
+  onArrySizeChange(e: any) {
     const value = e.target.value;
     this.arrService.generateNewArr(value);
   }
 
-  onSpeedChange(e: any){
+  onSpeedChange(e: any) {
     const value = e.target.value;
+    this.arrService.setSpeed(value);
+  }
+  makeAllBtnFalse() {
+    this.isBubbleSortSelected = false;
+    this.isMergeSortSelected = false;
+    this.isQuickSortSelected = false;
+    this.isHeapSortSelected = false;
+  }
+
+  onStartSorting() {
+    const speed = this.speed.nativeElement.value;
+    this.isSortingStarted = true;
+
+    if (this.isMergeSortSelected) {
+      console.log('start merge sorting');
+    } else if (this.isQuickSortSelected) {
+      console.log('start quick sorting');
+    } else if (this.isHeapSortSelected) {
+      console.log('start heap sorting');
+    } else if (this.isBubbleSortSelected) {
+      console.log('start bubble sorting');
+      this.arrService.startBubbleSort().then((isCompleted) => {
+        this.isSortingStarted = false;
+        this.isSortingDone = isCompleted;
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.arrSub?.unsubscribe();
   }
 }
--- src/app/services/arr-sort.service.ts
+++ src/app/services/arr-sort.service.ts
@@ -1,22 +1,89 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { BehaviorSubject, delay, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArraySortService {
   defaultArrLength = 40;
-  private arrSub = new BehaviorSubject<number[]>([]);
-  public arr$: Observable<number[]> = this.arrSub.asObservable();
+  speed = 50;
+
+  private arrStore: ArrayModel[] = [];
+  private arrSub = new BehaviorSubject<ArrayModel[]>([]);
+  public arr$: Observable<ArrayModel[]> = this.arrSub.asObservable();
 
   constructor() {
     this.generateNewArr();
   }
 
+  setSpeed(value: number) {
+    const lowestSpeed = 200;
+    this.speed = lowestSpeed - value;
+  }
+
   generateNewArr(length: number = this.defaultArrLength) {
-    const newArr = Array.from({ length: length * 2 }, () =>
-      Math.floor((Math.random() + 0.01) * 80)
+    const newArr = Array.from(
+      { length: length * 2 },
+      () =>
+        ({
+          value: Math.floor((Math.random() + 0.01) * 80),
+          isSelected: false,
+          isSorted: false,
+          isDone: false,
+        } as ArrayModel)
     );
     this.arrSub.next(newArr);
+    this.arrStore = newArr;
+  }
+
+  async startBubbleSort(): Promise<boolean> {
+    var n = this.arrStore.length;
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        await wait(this.speed);
+        this.arrStore[j].isSelected = true;
+        this.arrStore[j + 1].isSelected = true;
+        this.arrSub.next(this.arrStore);
+
+        if (this.arrStore[j].value > this.arrStore[j + 1].value) {
+          await wait(this.speed);
+
+          var temp = this.arrStore[j].value;
+          this.arrStore[j].value = this.arrStore[j + 1].value;
+          this.arrStore[j + 1].value = temp;
+
+          this.arrStore[j].isSelected = false;
+          this.arrStore[j + 1].isSelected = false;
+        } else {
+          await wait(this.speed);
+          this.arrStore[j].isSelected = false;
+          this.arrStore[j + 1].isSelected = false;
+          this.arrSub.next(this.arrStore);
+        }
+      }
+      await wait(this.speed);
+      this.arrStore[n - (i + 1)].isSelected = false;
+      this.arrStore[n - (i + 1)].isSorted = true;
+      this.arrSub.next(this.arrStore);
+    }
+
+    await wait(1000);
+    this.arrStore.forEach(async (x) => {
+      x.isSelected = false;
+      x.isSorted = false;
+      x.isDone = true;
+    });
+    this.arrSub.next(this.arrStore);
+
+    return Promise.resolve(true);
   }
 }
+
+export interface ArrayModel {
+  value: number;
+  isSelected: boolean;
+  isSorted: boolean;
+  isDone: boolean;
+}
+
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
